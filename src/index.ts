b441ee87e242630dc0d3f10abde4fff42b37760b
Mitchell Gildenberg <mgild@umich.edu>
2021-08-20 16:14:47 -0400

    publish hidden orca modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from "./public";
+export * from "./constants";
+export * from "./model/orca/pool/orca-pool";
+export * from "./model/orca/pool/pool-types";
+export * from "./model/orca/orca-impl";
+export * from "./model/orca-factory";
